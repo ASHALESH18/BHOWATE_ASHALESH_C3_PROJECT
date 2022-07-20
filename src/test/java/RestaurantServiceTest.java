b0d976063929f64cbda7fd2a1329d2887e4cf5e8
ASHALESH18 <ashaleshb18@gmail.com>
2022-07-21 00:52:18 +0530

    Part 2: Solution

--- src/test/java/RestaurantServiceTest.java
+++ src/test/java/RestaurantServiceTest.java
@@ -22,7 +22,7 @@ class RestaurantServiceTest {
     //>>>>>>>>>>>>>>>>>>>>>>SEARCHING<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void searching_for_existing_restaurant_should_return_expected_restaurant_object() throws restaurantNotFoundException {
-        //WRITE UNIT TEST CASE HERE
+
         Restaurant restaurant1 =  service.findRestaurantByName("Amelie's cafe");
         assertNotNull(restaurant1);
     }
